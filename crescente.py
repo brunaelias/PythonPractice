84f7b51fab60e4c71228dd5c26c6692bdcf0b4af
brunaelias <70407982+brunaelias@users.noreply.github.com>
2021-06-08 12:33:38 -0300

    Adding base files.

---
+++ crescente.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Insira um numero inteiro:"))
+numero2 = int(input("Insira um numero inteiro:"))
+numero3 = int(input("Insira um numero inteiro:"))
+if (numero1 < numero2 < numero3):
+	print("Crescente")
+else:
+	print("Não está em ordem crescente")
---
+++ quadrado.py
@@ -0,0 +1,4 @@
+quadradoLado = int (input ("insira o lado do quadrado aqui: "))
+perimetro = quadradoLado * 4
+area = quadradoLado * quadradoLado
+print ("perímetro:", perimetro, "- área:", area)
